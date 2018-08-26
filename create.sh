702572ae358094a81d6bcecc0aef44fa86e5ea9c
Necromancer <necromancerr@users.noreply.github.com>
2018-08-26 21:21:39 +0200

    first commit

---
+++ create.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#mknod /dev/loop0 b 7 0
+mkdir /mnt/iso1
+mount -o loop /iso/coreos-setup-template.iso /mnt/iso1
+cp -R /mnt/iso1 /tmp_iso
+mkdir /tmp_iso/profiles
+
+ls -1 /profiles | sed 's/\.yaml$//g' | xargs -n1 -I{} ct -in-file "/profiles/{}.yaml" -out-file "/tmp_iso/profiles/{}.ign"
+xorrisofs -V "COREOS SETUP" -follow-links -J -l -R -b boot/syslinux/isolinux.bin -c boot/syslinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -o /iso/coreos-setup.iso /tmp_iso/ && isohybrid /iso/coreos-setup.iso
